a5464eeccd106923476ea41879e93b4d00e42d08
hazem elhalawany <hazem.elhalawany@gmail.com>
2026-06-14 15:13:02 +0300

    sds

---
+++ src/test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#src/test.sh
+EXPECTED_OUTPUT="Hello, Test!"
+
+OUTPUT=$(node -e "console.log(require('./app').greet('Test'))")
+
+if [ "$OUTPUT" == "$EXPECTED" ]; then
+  echo "Test passed!" 
+  exit 0
+
+else
+  echo "Test failed!"
+  echo "Expected: $EXPECTED_OUTPUT"
+  echo "Got: $OUTPUT"
+
+  exit 1
+  
+fi
+
